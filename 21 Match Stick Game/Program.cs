022f77ca7672494fe2b113449d621e5de24dc859
Sameer Shaikh <60113690+sameer550@users.noreply.github.com>
2020-02-21 17:06:43 +0500

    Add files via upload
    Console Application Game in C# 2019

---
+++ 21 Match Stick Game/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _21_Match_Stick_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int user; int comp;
+            int total = 21;
+            while (total>1)
+            {
+                
+                Console.WriteLine("User turn");
+                user = Convert.ToInt32(Console.ReadLine());
+                total -= user;
+                Console.WriteLine("Left = "+total);
+                comp = 5 - user;
+                Console.WriteLine("Computer Turn");
+                
+                total -= comp;
+                Console.WriteLine("Computer Selected = "+comp);
+                Console.WriteLine("Left = "+total);
+                
+
+
+            }
+            if (total == 1)
+            {
+                Console.WriteLine("You lose");
+            }
+
+
+        }
+    }
+}
